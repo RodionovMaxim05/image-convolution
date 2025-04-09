ad174280479d910f4adfb1c4d9097a046dc4d003
Maxim Rodionov <rodionov.maxim05@gmail.com>
2025-04-10 00:18:48 +0300

    refactor: 'main.c' and 'test_main.c'

--- tests/test_main.c
+++ tests/test_main.c
@@ -3,6 +3,7 @@
 #include <stddef.h>
 
 #include <cmocka.h>
+#include <time.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
@@ -14,16 +15,22 @@
 #include "unit_tests.c"
 
 int main(void) {
-	const struct CMUnitTest tests[] = {
+	srand((unsigned int)time(NULL));
+
+	const struct CMUnitTest core_tests[] = {
 		cmocka_unit_test(test_create_filter),
 		cmocka_unit_test(test_split_assemble_channels),
 		cmocka_unit_test(test_identity_filter),
+	};
 
+	const struct CMUnitTest sequential_tests[] = {
 		cmocka_unit_test(test_filter_inverse_with_random_image),
 		cmocka_unit_test(test_filter_inverse_with_default_image),
 		cmocka_unit_test(test_filter_zero_padding_with_random_image),
 		cmocka_unit_test(test_filter_zero_padding_with_default_image),
+	};
 
+	const struct CMUnitTest parallel_tests[] = {
 		cmocka_unit_test(test_parallel_pixel_with_default_image),
 		cmocka_unit_test(test_parallel_pixel_with_random_image),
 		cmocka_unit_test(test_parallel_row_with_default_image),
@@ -34,5 +41,13 @@ int main(void) {
 		cmocka_unit_test(test_parallel_block_with_random_image),
 	};
 
-	return cmocka_run_group_tests(tests, NULL, NULL);
+	int status = 0;
+	status |= cmocka_run_group_tests_name("Core Functionality Tests", core_tests,
+										  NULL, NULL);
+	status |= cmocka_run_group_tests_name("Sequential Application Tests",
+										  sequential_tests, NULL, NULL);
+	status |= cmocka_run_group_tests_name("Parallel Application Tests",
+										  parallel_tests, NULL, NULL);
+
+	return status;
 }
